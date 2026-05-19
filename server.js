daff2181a2bbd10a9c2838ec156451676eec734d
mikedojcin-cell <mikedojcin@googlemail.com>
2026-05-18 22:53:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -331,7 +331,7 @@ app.post('/api/graph', async (req, res) => {
 
 app.get('/auth/meta', (req, res) => {
   const { clientId } = req.query;
-  const scope = 'pages_show_list,business_management,pages_read_engagement,pages_manage_posts,instagram_manage_insights';
+  const scope = 'pages_show_list,business_management';
   const url = `https://www.facebook.com/v19.0/dialog/oauth?client_id=${META_APP_ID}&redirect_uri=${encodeURIComponent(META_REDIRECT)}&scope=${scope}&state=${clientId}`;
   res.redirect(url);
 });
