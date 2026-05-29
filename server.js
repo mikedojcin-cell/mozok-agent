10becb9f530495b892d37e4a11e794891f48fcdb
mikedojcin-cell <mikedojcin@googlemail.com>
2026-05-29 07:51:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const META_REDIRECT = 'https://app.mozok.co/auth/meta/callback';
 const BASE_URL = 'https://app.mozok.co';
 
 
+
 // âââ AUTH MIDDLEWARE ââââââââââââââââââââââââââââââââââââââââââââââââââââââââââ
 
 async function requireAuth(req, res, next) {
